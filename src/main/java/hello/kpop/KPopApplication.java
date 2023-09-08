04a91ebae07b55da9e5917ce1470d59ee8e8df78
dayeon <chuen1234@naver.com>
2023-09-08 19:15:17 +0900

    first commit

---
+++ src/main/java/hello/kpop/KPopApplication.java
@@ -0,0 +1,13 @@
+package hello.kpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KPopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KPopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/kpop/KPopApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.kpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KPopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
