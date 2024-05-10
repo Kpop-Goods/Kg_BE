fdf82ded11aab01fe3c9b185e0ae6dac1b3f398f
[hhs] <hwazxcv@naver.com>
2024-05-10 17:38:27 +0900

    임시 저장

--- src/main/java/hello/kpop/admin/controllers/AdminController.java
+++ src/main/java/hello/kpop/admin/controllers/AdminController.java
@@ -1,10 +1,8 @@
 package hello.kpop.admin.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +14,7 @@ public class AdminController {
 
 
     @GetMapping("/home")
-    public String index(@ModelAttribute Authentication authentication) {
+    public String index() {
 
 
 
